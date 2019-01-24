d1011b6147ddd8d946e4083b326e58ef61e305e7
waynecz <451578533@qq.com>
2019-01-24 18:29:02 +0800

    fix: replace ico to grisoso cdn

--- src/chrome/background/background.js
+++ src/chrome/background/background.js
@@ -73,7 +73,7 @@ chrome.runtime.onInstalled.addListener(async reason => {
     const { version } = require('@/manifest.json')
     chrome.notifications.clear('updateInfo')
     chrome.notifications.create('updateInfo', {
-      iconUrl: 'http://p5grwrmf4.bkt.clouddn.com/dadda-ico.png',
+      iconUrl: 'https://cdn.grisoso.com/daddadadda-ico.png',
       type: 'basic',
       title: `${version} 更新：`,
       message: require('@/changelog-breif.json')[version] || '点击查看更新内容',
