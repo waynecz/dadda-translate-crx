dab688bd3bef950bbfce35ce016206c84717581d
大狗 <704274491@qq.com>
2019-09-23 07:46:08 +0800

    fix: 修复了设置页面前进后退页面不更新的问题 (#225)

--- src/components/react/App.js
+++ src/components/react/App.js
@@ -25,6 +25,10 @@ class App extends Component {
   }
 
   async componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const { link } = _parseQuery()
+      this.changeRoute(link)
+    });
     const vocabulary = await VocabularyMachine.get()
     const { dispatch, currentLink } = this.props
 
